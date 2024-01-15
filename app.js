9c4200f42ee6966668508b1c39e0279be722cf83
RANGARAJAN, SRINIVASAN <sr953f@att.com>
2024-01-15 15:09:25 +0530

    sccripts added for start and build

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React"
-);
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", { id: "heading" }, "Im a heading from child 1"),
-    React.createElement("h2", { id: "heading" }, "Im a heading2 from child 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", { id: "heading" }, "Im a heading from child 2"),
-    React.createElement("h2", { id: "heading" }, "Im a heading2 from child 2"),
-  ]),
-]);
-console.log(heading);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", xyz: "abc" },
+//   "Hello World from React"
+// );
+const jsxHeading = <h1>Hello React!</h1>
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<h1>Hello React!</h1>)
