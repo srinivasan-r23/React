7d031bb83493bb0ebb8dabc24e1f30decfcfd30f
RANGARAJAN, SRINIVASAN <sr953f@att.com>
2024-01-15 16:31:24 +0530

    React Element and Component

--- app.js
+++ app.js
@@ -6,6 +6,42 @@ import ReactDOM from "react-dom/client";
 //   { id: "heading", xyz: "abc" },
 //   "Hello World from React"
 // );
-const jsxHeading = <h1>Hello React!</h1>
+
+const CounterComponent = (initialCount) => {
+  // State management using closure
+  let count = initialCount || 0;
+
+  // The returned function represents the render function
+    return (
+      <div>
+        <p>Count: {count}</p>
+        <button onClick={() => count++}>Increment</button>
+      </div>
+    );
+};
+const jsxHeading = <h1>Hello React!</h1>;
+const Counter1 = CounterComponent(1); //I.  call function and assingn to element
+console.log(Counter1)
+const Component1 = () => {
+  return (
+    <div>
+   I'm from component but coming from from element 
+      
+    </div>
+  );
+};
+const ele = <span><Component1 /></span>
+const TitleComponent = () => {
+  return (
+    <div>
+      <h1>Counter 1</h1>
+      {/* {<CounterComponent initialCount={1} />}  if call as component, then make this component to accept and read the props via props.*/}
+      {Counter1} 
+      {/* I. call the react element like this */}
+      <h1>Counter 2</h1>
+      {ele}
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello React!</h1>)
+root.render(<TitleComponent />);
