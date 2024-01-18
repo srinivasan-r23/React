4da8ba1b7306a700260883950f3b143b1c919fb5
RANGARAJAN, SRINIVASAN <sr953f@att.com>
2024-01-18 13:30:47 +0530

    search and filter feature introduced

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import { resList } from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
+  const [listOfRestaurant, setListOfRestaurant] = useState(resList);
+  const filterHandler = () => {
+    const filteredRestaurant = resList?.filter(
+      (res) => res?.info?.avgRating >= 4
+    );
+    setListOfRestaurant(filteredRestaurant);
+  };
+  const searchHandler = (e) => {
+    const query = e.target.value;
+    const filteredRestaurant = resList?.filter((res) =>
+      res?.info?.name?.toLowerCase()?.includes(query)
+    );
+    setListOfRestaurant(filteredRestaurant);
+  };
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={filterHandler}>
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="search">
+        <input type="text" className="search-input" onChange={searchHandler} />
+      </div>
       <div className="res-container">
-        {resList?.map((res) => (
+        {listOfRestaurant?.map((res) => (
           <RestaurantCard resData={res} key={res?.info?.id} />
         ))}
       </div>
